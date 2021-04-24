ce48bd0558ddc0cbd572b48c510a3e9734b5b59f
JasonHarig <Jason.W.Harig@gmail.com>
2021-04-24 18:01:02 -0400

    Add files via upload

---
+++ FizzBuzzWhileLoop.js
@@ -0,0 +1,24 @@
+// Using while loop
+
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+
+  while (count < 101) {
+    if (count % 3 === 0 && count % 5 === 0) {
+      output.push("FizzBuzz");
+    } else if (count % 3 === 0) {
+      output.push("Fizz")
+    } else if (count % 5 === 0) {
+      output.push("Buzz")
+    } else {
+      output.push(count);
+    }
+
+    count++;
+  }
+  console.log(output);
+
+}
+fizzBuzz();
